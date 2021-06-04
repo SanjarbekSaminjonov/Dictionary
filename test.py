dbf2e2a5f500909885c6431a2c96dc67d984a8ff
SanjarbekSaminjonov <sanjarbeksaminjonovv@gmail.com>
2021-06-04 23:54:09 +0500

    1

--- test.py
+++ test.py
@@ -1,30 +1,45 @@
 import os
+from random import choice
 
-if not os.path.exists('test.txt') :
-    file = open('test.txt', 'x')
+fileName = 'units.txt'
+
+if not os.path.exists(fileName) :
+    file = open(fileName, 'x')
     file.close()
 
 def writer(words):
     file.write(words)
 
+clearConsole = lambda: os.system('cls' if os.name in ('nt', 'dos') else 'clear')
 
 print('1.Add words\n2.Learn by heart')
 
 if input('>> Enter the command: ') == '1':
     print("Enter the words in the 'En-Uz' view:\n(enter 'done' to finish)")
     while 1:
-        file = open('test.txt', 'a')
+        file = open(fileName, 'a')
         word = input('>>> ')
-        if word == 'done' : file.close(); break
+        if word == 'done' :
+            file.close(); break
         writer(word + '\n')
 
 else:
-    print('We are beginning...\nPress enter___')
-    file = open('test.txt')
+    words = []
+    file = open(fileName)
+    for i in file:
+        words.append(i)
+    file.close()
 
-    for line in file:
-        print(line.split('-')[0])
+    while 1:
+        if len(words) == 0 : break
+
+        line = choice(words)
+        words.remove(line)
+        clearConsole()
+        print(line.split('-')[1].strip(), end='')
         if input() == "" :
-            print(line.split('-')[1])
-        if input() == "" : continue
+            print(line.split('-')[0])
+        if input() == "" :
+            clearConsole()
+            continue
         if input() == "end" : break
